f2de9c391ae6dc52e0e4efdfbca31299be425ecc
Matias.E.Garcia <matiasegarcia22@gmail.com>
2022-11-22 17:15:49 -0300

    first commit

---
+++ src/main/java/com/practice/h2springBoot/H2springBootApplication.java
@@ -0,0 +1,13 @@
+package com.practice.h2springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2springBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2springBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/h2springBoot/H2springBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.h2springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2springBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
